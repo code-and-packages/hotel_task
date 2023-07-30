8d2bb67186fe9ee12b5bc5f907fc0a85e254dace
unknown <devmostafa>
2023-07-30 21:38:32 +0300

    tests

---
+++ tests/Unit/AdvertisedHotelRoomsTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class AdvertisedHotelRoomsTest extends TestCase {
+
+    /* .\vendor\bin\phpunit .\tests\Unit\AdvertisedHotelRoomsTest.php */
+
+    public function testTrueIsTrue() {
+        $this->assertTrue(true);
+    }
+
+
+}
